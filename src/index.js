78c72c8c5000bda901bf9b614e080a4d4cc33ec7
raghavmanish <mnsyd24@gmail.com>
2024-11-14 16:00:32 +0530

    again implimented and tested nodemailer and then set a api for creating ticket

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
 const { ServerConfig } = require('./config');
+// const { EmailController } = require("./controllers") ;
+const apiRoutes = require("./routes") ;
 
 const mailsender = require("./config/email-config")
 const app = express();
@@ -10,16 +12,18 @@ app.use(express.urlencoded({extended:true})) ;
 
 app.listen(ServerConfig.PORT, async () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
-    try {
-        const response = await mailsender.sendMail({
-            from: "airlinenotification89@gmail.com",
-            to: "mnsyd24@gmail.com",
-            subject: "12-11-2024 ....tell me is it working",
-            text: "12-11-2024 ....yes it is working",
-        });
-        console.log("Queue is up");
-        console.log("Mail response:", response);
-    } catch (error) {
-        console.log("Mail sending error:---> ", error);
-    }
+    // try {
+    //     const response = await mailsender.sendMail({
+    //         from: "airlinenotification89@gmail.com",
+    //         to: "mnsyd24@gmail.com",
+    //         subject: "12-11-2024 ....tell me is it working",
+    //         text: "12-11-2024 ....yes it is working",
+    //     });
+    //     console.log("Queue is up");
+    //     console.log("Mail response:", response);
+    // } catch (error) {
+    //     console.log("Mail sending error:---> ", error);
+    // }
 }); 
+
+app.use('/api' , apiRoutes ) ;
